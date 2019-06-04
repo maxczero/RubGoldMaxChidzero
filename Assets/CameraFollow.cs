e35d70655bee81c7d8f63256d576172218530279
Max Chidzero <mwc349@nyu.edu>
2019-06-03 21:33:30 -0400

    First Commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void LateUpdate () {
+    
+    transform.position = target.position + offset;
+    
+        
+    }
+}
